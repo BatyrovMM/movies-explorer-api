daff0c311faa83cb0e3307cec8bf53bae1481be5
Malik Batyrov <batyrov.m.m@yandex.ru>
2021-03-24 12:49:48 +0300

    Begin

--- routes/movie.js
+++ routes/movie.js
@@ -8,7 +8,20 @@ const {
 
 movie.get('/movie', getMovies);
 
-movie.post('/movie', createMovie);
+movie.post('/movie', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().min(1).required(true),
+    director: Joi.string().min(1).required(true),
+    duration: Joi.number().min(1).required(true),
+    year: Joi.string().min(2).max(4).required(true),
+    description: Joi.string().min(1).required(true),
+    image: Joi.string().pattern(new RegExp(/https?:\/\/w{0,3}[a-z0-9-._~:\/?#[\]@!$&'()*+,;=]{0,}/i)).required(true),
+    trailer: Joi.string().pattern(new RegExp(/https?:\/\/w{0,3}[a-z0-9-._~:\/?#[\]@!$&'()*+,;=]{0,}/i)).required(true),
+    thumbnail: Joi.string().pattern(new RegExp(/https?:\/\/w{0,3}[a-z0-9-._~:\/?#[\]@!$&'()*+,;=]{0,}/i)).required(true),
+    nameRU: Joi.string().required(true),
+    nameEN: Joi.string().required(true),
+  }),
+}), createMovie);
 
 movie.delete('/movie/:movieId', celebrate({
   params: Joi.object().keys({
--- routes/users.js
+++ routes/users.js
@@ -8,16 +8,8 @@ const {
 users.get('/users/me', myUser);
 users.patch('/users/me', celebrate({
   body: Joi.object().keys({
-    country: Joi.string().min(1).required(true),
-    director: Joi.string().min(1).required(true),
-    duration: Joi.number().min(1).required(true),
-    year: Joi.string().min(2).max(4).required(true),
-    description: Joi.string().min(1).required(true),
-    image: Joi.string().pattern(new RegExp(/https?:\/\/w{0,3}[a-z0-9-._~:\/?#[\]@!$&'()*+,;=]{0,}/i)).required(true),
-    trailer: Joi.string().pattern(new RegExp(/https?:\/\/w{0,3}[a-z0-9-._~:\/?#[\]@!$&'()*+,;=]{0,}/i)).required(true),
-    thumbnail: Joi.string().pattern(new RegExp(/https?:\/\/w{0,3}[a-z0-9-._~:\/?#[\]@!$&'()*+,;=]{0,}/i)).required(true),
-    nameRU: Joi.string().required(true),
-    nameEN: Joi.string().required(true),
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), myUserUpdateInfo);
 
